f46b8689dd120e156f2c7aed811a682c88d3603c
Shunsuke Shigemitsu <choge.2nd@gmail.com>
2015-04-15 21:01:32 +0900

    fasta_manager has been merged into dataset_maker.py

--- predictor/dataset_maker.py
+++ predictor/dataset_maker.py
@@ -3,11 +3,114 @@
 """DataSetMaker データセットを作る。
 
 """
+from os.path import splitext, basename
+import re
 
 from . import dataset
-from os.path import splitext, basename
 from . import fasta_manager
+from . import fasta
 
+class FastaBuilder( object ):
+    """Manager プロトタイプを登録・管理するクラス。
+
+    値の登録、登録された値からの適切なプロトタイプの選択などの
+    基本的な機能を実装しておく。"""
+
+    def __init__(self, default):
+        """コンストラクタ。
+        """
+        self.regex = {} # 正規表現を持つ。
+        self.prototypes = {} #
+        self.default_prototype = default
+
+    def register(self, prototype_name, prototype, regex):
+        """prototype_nameに対応するクラスと正規表現を
+        登録する。すでに登録してある場合は、エラー。"""
+        if prototype_name in self.prototypes or prototype_name in self.regex:
+            raise ValueError(prototype_name + " already registered.")
+        if isinstance(regex, str):
+            regex = re.compile( regex )
+        if not isinstance(prototype, type):
+            raise TypeError(prototype + " is not a Fasta object.")
+        self.prototypes[ prototype_name ] = prototype
+        self.regex[ prototype_name ] = regex
+
+    def guess_database(self, unknown_str):
+        """unknown_strがどのプロトタイプに適合するのかを
+        判定し、適切なプロトタイプを返す。
+
+        @unknown_str Fasta配列そのものを想定。"""
+        # 最初の行のみを取得
+        unknown_str = unknown_str.split("\n", 1)[0]
+        if len( unknown_str ) == 0:
+            raise ValueError("Empty string.")
+        elif unknown_str[0] != '>':
+            raise ValueError("")
+        for prototype_name, regex in list(self.regex.items()):
+            result = regex.match( unknown_str )
+            if result: # 正規表現がマッチした場合
+                return self.prototypes[ prototype_name ]
+        # ここまで処理が流れてくると、どのプロトタイプにもマッチしなかった
+        # ということ。デフォルトのプロトタイプを返す。
+        return self.default_prototype
+
+    def create(self, unknown_str):
+        """インスタンスを生成するメソッド。
+        """
+        prototype = self.guess_database(unknown_str)
+        return prototype( unknown_str )
+
+    def new_class(self, classname, baseclass, attr, regex):
+        """動的に新しいクラスを生成して、登録します。
+
+        @classname 新たなクラスの名前。既存のものと衝突しないものである
+        必要があります。
+        ＠baseclass 基底クラス。通常はFastaまたはProteinFastaあたりを指定
+        すると良いと思います。
+        @attr そのクラスが持つ属性値。re_identifierなどの正規表現を登録する
+        必要があります。
+        @regex 新たなクラスであるかどうかを判別するための正規表現です。
+        """
+        # 名前の衝突をチェック
+        if classname in self.prototypes:
+            raise ValueError(classname + "は既に登録されています。")
+        # 正規表現は新たに登録する必要がある。
+        required_attrs = ['re_identifier', 're_accession', 're_organism']
+        for required_attr in required_attrs:
+            if not required_attr in attr:
+                raise ValueError(required_attr + "は必須です。")
+        new_class = type( classname, baseclass, attr )
+        self.register(classname, new_class, regex)
+
+class ProteinFastaBuilder( FastaBuilder ):
+    """ProteinFastaManager アミノ酸配列のFastaオブジェクトを作成する。
+
+    あらかじめよく使うクラスを登録しておく。"""
+
+    def __init__(self):
+        """コンストラクタ"""
+        self.regex = {}
+        self.prototypes = {}
+        self.default_prototype = fasta.BasicProteinFasta
+
+        # SwissProt
+        self.new_class('SwissProt', (fasta.BasicProteinFasta,), {
+            're_identifier' : re.compile("^>sp\|[^\|]+\|(\S+) .*"),
+            're_accession'  : re.compile("^>sp\|([^\|]+)\|.*"),
+            're_organism'   : re.compile(".* OS=(\w+ \w+(?: [^=]+)?) .*"),
+            }, re.compile("^>sp") )
+        # SwissProt
+        self.new_class('TrEMBLE', (fasta.BasicProteinFasta,), {
+            're_identifier' : re.compile("^>tr\|[^\|]+\|(\S+) .*"),
+            're_accession'  : re.compile("^>tr\|([^\|]+)\|.*"),
+            're_organism'   : re.compile(".* OS=(\w+ \w+(?: [^=]+)?) .*"),
+            }, re.compile("^>tr") )
+        # GenBank / RefSeq
+        self.new_class('GenBank_refseq', (fasta.BasicProteinFasta, ), {
+            're_identifier' : re.compile("^>gi\|\d+\|ref\|([^\|]+)\|? .*"),
+            're_accession'  : re.compile("^>gi\|(\d+)\|ref.*"),
+            're_organism'   : re.compile(".* \[(\w+ \w+(?: \w+)?)\].*"),
+            }, re.compile("^>gi\|\d+\|ref") )
 
 class DataSetMaker(object):
     """DataSetMaker データセットを作るためのクラス。
@@ -109,14 +212,19 @@ class FastaReader( object ):
         seq = ''
         fasta_list = []
         line_no = 0
-        for line in s:
+        for line in s.splitlines():
             line_no += 1
+            print(line_no, line)
             if line == '' or line == "\n" or line[0] == '#':
                 continue
             elif len( seq ) > 0 and line[0] == '>':
                 #seq_num += 1
+                print(len(fasta_list), seq)
                 fasta_list.append( self.manager.create(seq) )
-                seq = line
+                seq = line + "\n"
+            elif line[0] == '>':
+                # the first line
+                seq += line + "\n"
             else:
                 seq += line
         ## Add the last line
@@ -139,7 +247,7 @@ class FastaDataSetMaker(DataSetMaker):
     """FastaDataSetMaker Fastaファイルから新しいデータセットを作る。
     """
 
-    def __init__(self, builder=fasta_manager.ProteinFastaManager,
+    def __init__(self, builder=ProteinFastaBuilder,
                        reader=FastaReader,
                        data_type=dataset.FastaDataSet):
         """builderはFastaMakerあるいはFastaManager。
