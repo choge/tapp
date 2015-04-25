d92da48402180ef12a38bba98124dc00b625c862
Shunsuke Shigemitsu <choge.2nd@gmail.com>
2015-04-25 18:58:40 +0900

    Added a handler for ajax request (which will return predicted results)

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import tornado.web
 import tornado.gen
 import os
 import os.path
-import momoko
+import uuid
+import shelve
+import json
+import numpy
 
 from tornado.options import define, options
 
@@ -34,28 +37,20 @@ class QueryHandler(tornado.web.RequestHandler):
         query = self.get_argument('query')
         dataset_maker = predictor.FastaDataSetMaker()
         query_data = dataset_maker.read_from_string(query)
+        identifier = str(uuid.uuid4())
+        self.application.db[identifier] = query_data
         
-        self.render('result.html', results=predicted)
-
-    def async_predict(self, myhmm, dataset, reverse=True, callback=None):
-        """Async wrapper for predict.
-        Though usually myhmm.predict() doesn't take much time, 
-        make it asynchrounous would be better as for performance."""
-        callback(myhmm.predict(dataset, reverse))
-        
+        self.redirect("result/{0}".format(identifier), permanent=True)
 
 class QueryAPIHandler(tornado.web.RequestHandler):
     """QueryAPIHandler  will return the result in JSON
     
     TODO: write"""
     
-    @tornado.web.asynchronous
     @tornado.gen.coroutine
-    def post(self):
+    def get(self, query_id):
         """returns calculate"""
-        query = self.get_argument('query')
-        dataset_maker = predictor.FastaDataSetMaker()
-        query_data = dataset_maker.read_from_string(query)
+        query_data = self.application.db[query_id]
         myhmm = predictor.MyHmmPredictor(
                 filename=os.path.join(
                     os.path.dirname(__file__), 'modelsFinal/ta4.xml'))
@@ -63,7 +58,8 @@ class QueryAPIHandler(tornado.web.RequestHandler):
         predicted = yield tornado.gen.Task(self.async_predict, 
                 myhmm, query_data, True)
         
-        self.render('result.html', results=predicted)
+        predicted = self.convert_numpy_types(predicted)
+        self.write(json.dumps(predicted))
 
     def async_predict(self, myhmm, dataset, reverse=True, callback=None):
         """Async wrapper for predict.
@@ -71,32 +67,45 @@ class QueryAPIHandler(tornado.web.RequestHandler):
         make it asynchrounous would be better as for performance."""
         callback(myhmm.predict(dataset, reverse))
 
+    def convert_numpy_types(self, predicted):
+        """As numpy types such as 'numpy.int64' cannot be converted into 
+        JSON format, so make these values into native python values.
+        
+        @param predicted  is a dictionary of the predicted result.
+        predicted
+        +[seq_id]
+         +['path'] : str, decoded path
+         +['pathnum'] : list of numpy.int64
+         +['likelihood'] : numpy.float64
+         +['omega'] : list of numpy.float64. """
+        new_result = {}
+        for seq_id, dic in predicted.items():
+            converted = {}
+            converted['pathnum'] = [i.item() for i in dic['pathnum']]
+            converted['omega'] = [i.item() for i in dic['omega']]
+            converted['likelihood'] = dic['likelihood'].item()
+            converted['path'] = dic['path']
+            new_result[seq_id] = converted
+        return new_result
+        
+
 class ResultPageHandler(tornado.web.RequestHandler):
     """ResultPageHandler"""
 
     def get(self, result_id):
         """show the result page, which is stored in DB."""
-        pass
+        query_data = self.application.db[result_id]
+        self.render('result.html', query_id=result_id, query_data=query_data)
 
 class Application(tornado.web.Application):
     """Web app"""
     def __init__(self):
         handlers = [(r'/', TopPageHandler),
                     (r'/predict', QueryHandler),
-                    (r'/result/(\w+)', ResultPageHandler)]
-        db_database = 'tapp'
-        db_user = 'choge'
-        db_pass = 'postgres'
-        db_host = 'localhost'
-        db_port = 5432
-        dsn = 'dbname=%s user=%s password=%s host=%s port=%s' % (
-            db_database, db_user, db_pass, db_host, db_port)
-        self.db = momoko.Pool(
-                dsn=dsn,
-                size=1,
-                max_size=3,
-                setsession=('SET TIME ZONE UTC')
-                )
+                    (r'/predict/([\w\-]+)', QueryAPIHandler),
+                    (r'/result/([\w\-]+)', ResultPageHandler)]
+        self.db = shelve.open(os.path.join(
+            os.path.dirname(__file__), 'queries/db'))
         current_file_path = os.path.dirname(__file__)
         template_path = os.path.join(current_file_path, 'templates')
         static_path = os.path.join(current_file_path, 'static')
@@ -106,6 +115,9 @@ class Application(tornado.web.Application):
                 static_path=static_path,
                 debug=True)
 
+    def __del__(self):
+        if self.db is not None:
+            self.db.close()
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
