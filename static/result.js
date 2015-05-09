841ca637edb2dc208b0c66a90edee5adba5f69a4
Shunsuke Shigemitsu <choge.2nd@gmail.com>
2015-05-09 09:22:20 +0900

    email validation

--- static/result.js
+++ static/result.js
@@ -32,8 +32,16 @@ $(function () {
 
   // setup an email address
   $('#mailsend').click(function() {
+    // check if there is a valid mail address
+    if (! $('#email').text().match(/[^@]+@[^@]+/)) {
+      alert('Please enter a valid address');
+    }
+    // send mail
     $.post('../mail/' + $('#query_id').text(),
         {"email": $('#email').val()});
+    // disable the button
+    $('#mailsend').text('Mail sent');
+    $('#mailsend').addClass('disabled');
   });
 
   // retreieve date before requesting result
